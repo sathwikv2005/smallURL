9a1b714d61b6b8b6fd41906fb4431d869918a383
sathwikv2005 <sathwikv2005@gmail.com>
2025-03-04 18:32:18 +0530

    Update passportConfig.js

--- passportConfig.js
+++ passportConfig.js
@@ -8,8 +8,7 @@ module.exports = (app) => {
 			{
 				clientID: process.env.GOOGLE_CLIENT_ID,
 				clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-				callbackURL:
-					process.env.GOOGLE_CALLBACK_URL || 'http://127.0.0.1:9000/auth/google/callback',
+				callbackURL: '/auth/google/callback',
 			},
 			async (accessToken, refreshToken, profile, done) => {
 				try {
